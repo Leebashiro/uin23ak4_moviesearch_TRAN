48597f855409658f120f28688e48ee31a7a78170
Leebashiro <111729362+Leebashiro@users.noreply.github.com>
2023-02-28 09:16:17 +0100

    setup

---
+++ mstran/src/components/Layout.js
@@ -0,0 +1,19 @@
+export default function Layout(){
+    return (
+        <div id="container">
+          <header>
+            <Link to="/">
+                <h1>Movie Search</h1>
+            </Link>
+          </header>
+          <main>
+           <div id="bar">
+            
+           </div>
+          </main>
+          <footer>
+    
+          </footer>
+        </div>
+    )
+}
