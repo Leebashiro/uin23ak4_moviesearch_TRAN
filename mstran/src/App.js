9c5f31a50d3ca1a008517576865a4ddc73797213
Leebashiro <111729362+Leebashiro@users.noreply.github.com>
2023-03-19 19:19:47 +0100

    kommentar

--- mstran/src/App.js
+++ mstran/src/App.js
@@ -14,17 +14,18 @@ function App() {
     const data = await response.json()
   /*Først og fremst henter vi data fra API via fetch (på search får vi bare data på poster, title og year), og setter inn search 
   som parameter slik at vi får brukt search for å søke frem data i API. Videre, setter jeg også inn wildcard character (*)
-  etter paramteren slik at hvis jeg f.eks søker på  /
+  etter parameteren slik at hvis jeg f.eks søker på big, så vil søkeresultatet inkludere ord som starter med big*/
     
   /*Jeg skjønte ikke helt hvordan jeg skulle få til å få mer informasjon fra det samme API-kallet, så jeg bestemte meg for å
-  lage enda et API-kall. Denne henter fram ekstra informasjon som ble kalt i det første APi-kallet (f.eks genre, actors, osv)
+  lage enda et API-kall. Denne henter fram ekstra informasjon av posterne som ble kalt i det første APi-kallet.
   Setter inn en betingelse som sjekker om API faktisk gir noe data, slik at hele nettsiden ikke får noen error ved å ikke 
   fremstille data som ikke eksisterer*/
     if (data.Search && data.Search.length >= 1) {
-  /*I og med at vi henter ekstra-data til flere elementer, må
+  /*I og med at vi henter ekstra-data til flere elementer, kan
   vi bruke promise.all for å lage flere asynkroniske API-kall (via mapping) slik at de kan rendre samtidig på UI
   Bruker map for å lage flere promises som henter data fra API-kall med imdbID via url, slik at vi får data som f.eks genre,
-  director, awards, og lignende*/
+  director, awards, og lignende.
+  Inspirasjon: https://stackoverflow.com/questions/31710768/how-can-i-fetch-an-array-of-urls-with-promise-all*/
       const movieDetails = await Promise.all(
 
         data.Search.map(async (movie) => {
@@ -35,7 +36,7 @@ function App() {
         })
       )
   /*Hvis betingelsen ovenfor blir oppfylt, vil setMovies (oppdateringsvariabel) oppdatere seg med movieDetails som gir flere
-  detaljer om filmen. Hvis det motsatte skjer derimot så vil setMovies bli oppdatert med en tom array.*/
+  detaljer om filmen. Hvis det motsatte skjer derimot så vil setMovies bli oppdatert med en tom array (ingenting).*/
       setMovies(movieDetails)
     } else {
       setMovies([])
